4c9e2cd416c6de0366fd722d4cc09a0b9c1a65f8
Fedor Shubin <shubin.fedor@gmail.com>
2014-06-25 08:21:04 +0700

    add type<=>class map

--- soomla-native/projects/cocos2dx-android-core/src/com/soomla/cocos2dx/common/DomainHelper.java
+++ soomla-native/projects/cocos2dx-android-core/src/com/soomla/cocos2dx/common/DomainHelper.java
@@ -1,11 +1,14 @@
 package com.soomla.cocos2dx.common;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author vedi
@@ -16,6 +19,9 @@ public class DomainHelper {
 
     public static transient DomainHelper INSTANCE = null;
 
+    private Map<String, String> typeClassMap = new HashMap<String, String>();
+    private Map<String, String> classTypeMap = new HashMap<String, String>();
+
     public static DomainHelper getInstance() {
         if (INSTANCE == null) {
             synchronized (DomainHelper.class) {
@@ -25,6 +31,11 @@ public class DomainHelper {
             }
         }
         return INSTANCE;
+    }
+
+    public void registerTypeWithClassName(String type, String className) {
+        typeClassMap.put(type, className);
+        classTypeMap.put(className, type);
     }
 
     public <T> List<T> getDomainsFromJsonObjectList(List<JSONObject> jsonObjectList, String type) {
@@ -47,19 +58,31 @@ public class DomainHelper {
 
         List<JSONObject> jsonObjectList = new ArrayList<JSONObject>(domains.size());
         for (T domain : domains) {
-            try {
-                Method method = domain.getClass().getMethod("toJSONObject");
-                jsonObjectList.add((JSONObject) method.invoke(domain));
-            } catch (NoSuchMethodException e) {
-                throw new IllegalArgumentException(e);
-            } catch (InvocationTargetException e) {
-                throw new IllegalArgumentException(e);
-            } catch (IllegalAccessException e) {
-                throw new IllegalArgumentException(e);
-            }
+            jsonObjectList.add(domainToJsonObject(domain));
         }
 
         return jsonObjectList;
+    }
+
+    public JSONObject domainToJsonObject(Object domain) {
+        try {
+            Method method = domain.getClass().getMethod("toJSONObject");
+            final JSONObject jsonObject = (JSONObject) method.invoke(domain);
+            String type = jsonObject.optString(CommonConsts.BP_TYPE, "");
+            if (type == null) {
+                type = classTypeMap.get(domain.getClass().getName());
+                jsonObject.put(CommonConsts.BP_TYPE, type);
+            }
+            return jsonObject;
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException(e);
+        } catch (InvocationTargetException e) {
+            throw new IllegalArgumentException(e);
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException(e);
+        } catch (JSONException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
 }
